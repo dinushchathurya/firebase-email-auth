ce0febd97e1baf2d532f1ac97fac43bfce41e0f1
dinushchathurya <dinushchathurya21@gmail.com>
2020-07-11 10:11:02 +0530

    Update Environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,14 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyAzmklplXXY9peSS6ZPmGQV-PAHpck_rgw',
-    authDomain: 'fir-email-auth-24a0d.firebaseapp.com',
-    databaseURL: 'https://fir-email-auth-24a0d.firebaseio.com',
-    projectId: 'fir-email-auth-24a0d',
-    storageBucket: 'fir-email-auth-24a0d.appspot.com',
-    messagingSenderId: '647921537194',
-    appId: '1:647921537194:web:18b89b24d00f80ad3200d3',
-    measurementId: 'G-LVK1GJQFHV'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: ''
   }
 };
 
